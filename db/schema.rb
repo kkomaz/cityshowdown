b7ad2a7da7108d10c1f9a04c2073b287e79dfb29
bshap27 <rshapiro3@gmail.com>
2015-03-30 21:55:40 -0400

    added city, user, post, comment seeds to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330180946) do
+ActiveRecord::Schema.define(version: 20150331013900) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150330180946) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }, { name: 'New York' }])
-Post.create([{ tagline: 'NY Bagels', city_id: 3}])
+
+
+cities = ["New York", "Austin", "Los Angeles", "Chicago", "San Francisco", "Boston", "Seattle", "Portland"]
+cities.each {|city| City.create(name: city)}
+
+User.create(name: "Becca Shapiro", password: "test", email: "rshapiro3@gmail.com", username: "bshap27", city_id: 1)
+User.create(name: "Alex Lee", password: "test", email: "alexlee@gmail.com", username: "alee", city_id: 2)
+User.create(name: "Chloe Lee", password: "test", email: "chloelee@gmail.com", username: "clee", city_id: 3)
+
+Post.create(tagline: 'This city', description: "Is the best!", city_id: 1, user_id: 1)
+Post.create(tagline: 'No, MY city', description: "Is the best!", city_id: 2, user_id: 2)
+Post.create(tagline: 'Nah guys', description: "mine is totally the best!", city_id: 3, user_id: 3)
+
+Comment.create(content: "ok, whatever" , user_id: 1, post_id: 2)
+Comment.create(content: "ok, whatever" , user_id: 2, post_id: 3)
+Comment.create(content: "ok, whatever" , user_id: 3, post_id: 1)
