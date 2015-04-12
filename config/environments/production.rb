91fe6f681a09fec9e73e3fec92321a2d30ef595a
kkomaz <leeajl006@gmail.com>
2015-04-12 12:01:52 -0400

    paperclip modification test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => ENV['S3_BUCKET_NAME_PRODUCTION'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_KEY_ID']
     }
   }
 end
